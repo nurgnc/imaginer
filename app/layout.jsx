68b996e71bc2528de2736f38d3b2f11f31314b33
Bedriye Nur GENÇ <gencbedriyenur@gmail.com>
2024-01-25 23:53:20 +0300

    font libs added

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,9 +2,11 @@ import "@/styles/reset.css"
 import "@/styles/variables.css"
 import "@/styles/global.css"
 
+import { mainFont } from '@/libs/font'
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={mainFont.className}>
       <body className="layout">{children}
         <div className="overlay" />
       </body>
